81b376ad99dd2160c06ecb99be4909789f81a800
liucuilin <914988925@qq.com>
2017-07-19 10:33:56 +0800

    fix

--- js/myapp.js
+++ js/myapp.js
@@ -3,35 +3,37 @@ window.onload=function(){
   var $pause = document.getElementById("pause");
   var $play = document.getElementById("play");
 
-  $pause.onclick = function(){
-    if($myVideo.paused == false){
-      $myVideo.pause();
+  if ($myVideo && $pause && $play) {
+
+    $pause.onclick = function(){
+      if($myVideo.paused == false){
+        $myVideo.pause();
+      }
+      $play.setAttribute("class","on");
+      $pause.setAttribute("class","");
     }
-    $play.setAttribute("class","on");
-    $pause.setAttribute("class","");
-  }
 
-  $play.onclick = function(){
-    if($myVideo.paused == true){
-      $myVideo.play();
+    $play.onclick = function(){
+      if($myVideo.paused == true){
+        $myVideo.play();
+      }
+      $pause.setAttribute("class","on");
+      $play.setAttribute("class","");
     }
-    $pause.setAttribute("class","on");
-    $play.setAttribute("class","");
+
+    $myVideo.addEventListener('pause',function(){
+      $play.setAttribute("class","on");
+      $pause.setAttribute("class","");
+    })
+
+    $myVideo.addEventListener('play',function(){
+      $pause.setAttribute("class","on");
+      $play.setAttribute("class","");
+    })
+
+    $myVideo.addEventListener('ended',function(){
+      $pause.setAttribute("class","on");
+      $play.setAttribute("class","");
+    })
   }
-
-  $myVideo.addEventListener('pause',function(){
-    $play.setAttribute("class","on");
-    $pause.setAttribute("class","");
-  })
-
-  $myVideo.addEventListener('play',function(){
-    $pause.setAttribute("class","on");
-    $play.setAttribute("class","");
-  })
-
-  $myVideo.addEventListener('ended',function(){
-    $pause.setAttribute("class","on");
-    $play.setAttribute("class","");
-  })
-
 }
